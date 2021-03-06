733e464b93ea4ba2636a3924f67aeb78dd7a5e3e
ko-horiuchi <k.h0520@docomo.ne.jp>
2021-03-06 15:22:14 +0000

    make Order_detail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_142955) do
+ActiveRecord::Schema.define(version: 2021_03_06_151956) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -70,6 +70,14 @@ ActiveRecord::Schema.define(version: 2021_03_06_142955) do
     t.text "introduction"
     t.integer "price"
     t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "price"
+    t.integer "amount"
+    t.integer "making_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
