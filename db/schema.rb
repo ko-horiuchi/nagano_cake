118b327109a1a7075cc76787f1e5fe2d60cd3fe2
ko-horiuchi <k.h0520@docomo.ne.jp>
2021-03-06 14:25:55 +0000

    make Address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_142027) do
+ActiveRecord::Schema.define(version: 2021_03_06_142353) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "name"
+    t.string "postal_code"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
