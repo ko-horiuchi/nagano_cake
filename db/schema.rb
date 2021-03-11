27d2f3267591ff3b209364bd9ad968012be67bc4
ko-horiuchi <k.h0520@docomo.ne.jp>
2021-03-11 09:35:46 +0000

    [Add]item status column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_143156) do
+ActiveRecord::Schema.define(version: 2021_03_11_093319) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -72,10 +72,9 @@ ActiveRecord::Schema.define(version: 2021_03_09_143156) do
     t.string "image_id"
     t.text "introduction"
     t.integer "price"
-    t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "genre_id"
+    t.boolean "is_active", default: true, null: false
   end
 
   create_table "order_details", force: :cascade do |t|
